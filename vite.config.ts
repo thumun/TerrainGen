072ba5c2b034ea82746a9ad5a3f25efa2e02cb4c
Thomas Shaw <printer.83mph@gmail.com>
2025-11-07 13:06:19 -0500

    Add WebGPU types

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
+import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
