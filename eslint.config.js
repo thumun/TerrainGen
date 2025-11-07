14e7648bd1b4e915ca064b64fa371eba18a4bcc3
Thomas Shaw <printer.83mph@gmail.com>
2025-11-07 13:40:30 -0500

    Add custom eslint config, rework prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,47 @@
-//  @ts-check
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import importX from 'eslint-plugin-import-x';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
-import { tanstackConfig } from '@tanstack/eslint-config';
+const GLOB_EXCLUDE = [
+  '**/.nx/**',
+  '**/.svelte-kit/**',
+  '**/build/**',
+  '**/coverage/**',
+  '**/dist/**',
+  '**/snap/**',
+  '**/vite.config.*.timestamp-*.*',
+];
 
-export default [...tanstackConfig];
+export default defineConfig([
+  {
+    name: 'ignores',
+    ignores: GLOB_EXCLUDE,
+  },
+  {
+    name: 'javascript/typescript',
+    files: ['**/*.{js,ts,tsx}'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      reactHooks.configs.flat.recommended,
+      importX.flatConfigs.recommended,
+      importX.flatConfigs.typescript,
+    ],
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2020,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        parser: tseslint.parser,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {},
+  },
+]);
