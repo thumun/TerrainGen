77235682f94ecf23adcbd7d07db7c7c8df9a8018
Thomas Shaw <printer.83mph@gmail.com>
2025-11-15 11:24:05 -0500

    wip: remove group and bindings from UniformConfig

--- src/lib/shaders/jit/types/util.ts
+++ src/lib/shaders/jit/types/util.ts
@@ -1,9 +1,17 @@
 export type ReferenceKey = string;
 
-export type UniformConfig = {
-  type: 'f32' | 'u32' | 'vec3f';
+export type UniformConfigBase = {
   key: ReferenceKey;
-  group: number;
-  binding: number;
-  value: number | [number, number, number] | null;
 };
+
+type UniformConfigScalar = UniformConfigBase & {
+  type: 'f32' | 'u32';
+  initialValue: number;
+};
+
+type UniformConfigVec3 = UniformConfigBase & {
+  type: 'vec3f';
+  initialValue: [number, number, number];
+};
+
+export type UniformConfig = UniformConfigScalar | UniformConfigVec3;
