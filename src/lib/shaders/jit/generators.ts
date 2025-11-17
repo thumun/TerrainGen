be6f4a543582bf7b46441cafb5c2eb9879b89911
Thomas Shaw <printer.83mph@gmail.com>
2025-11-16 23:36:06 -0500

    fix tests and add todos for struct mode uniforms

--- src/lib/shaders/jit/generators.ts
+++ src/lib/shaders/jit/generators.ts
@@ -2,8 +2,12 @@ import * as shaderUtils from './shader-utils';
 import * as instructions from './types/instructions';
 import type * as util from './types/util';
 
-export function generateUniform(uniform: util.UniformConfig) {
-  return `@group(${uniform.group}) @binding(${uniform.binding}) var<uniform> ${uniform.key} : ${uniform.type};`;
+// TODO: this will likely have to change to use a struct instead
+export function generateUniform(
+  uniform: util.UniformConfig,
+  opts: { group: number; binding: number },
+) {
+  return `@group(${opts.group}) @binding(${opts.binding}) var<uniform> ${uniform.key} : ${uniform.type};`;
 }
 
 type ShaderUtil = () => string;
