dd046d20cbcf3293d13a57dc6cef2310124c958e
Thomas Shaw <printer.83mph@gmail.com>
2025-11-17 15:47:51 -0500

    Move node graph canvas back into @/components/editor

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -1,10 +1,10 @@
 import { useState, useRef } from 'react';
 
-import NodeGraphCanvas from './node-graph/node-graph-canvas';
+import NodeGraph from './node-graph';
+import NodeGraphCanvas from './node-graph-canvas';
 import TerrainCanvas from './terrain-canvas';
 import TerrainSliders from './terrain-sliders';
 
-import NodeGraph from '@/components/editor/node-graph';
 import { TerrainRenderer } from '@/lib/renderers/terrain-renderer';
 
 export default function Editor() {
