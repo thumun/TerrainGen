a25fd6a65e8d610cd7bd68b3862491c96d9835d7
Thomas Shaw <printer.83mph@gmail.com>
2025-11-20 22:06:29 -0500

    Fix auto-fixable problems

--- src/components/editor/node-graph/index.tsx
+++ src/components/editor/node-graph/index.tsx
@@ -13,11 +13,11 @@ import ContextMenu from './context-menu';
 
 import { nodeTypes } from '@/components/nodes';
 import { useContextMenu } from '@/hooks/use-context-menu';
+import { NodeDataProvider } from '@/hooks/use-node-data';
 import { useNodeGraph } from '@/hooks/use-node-graph';
 import * as graph from '@/lib/graph';
 import * as traversal from '@/lib/graph/traversal';
 import type * as scene from '@/lib/scene';
-import { NodeDataProvider } from '@/hooks/use-node-data';
 
 export const fitViewOptions: FitViewOptions = {
   padding: 0.2,
