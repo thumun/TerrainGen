836443c625ffa5cd0aa868e637dab31f73e0799c
Thomas Shaw <printer.83mph@gmail.com>
2025-11-20 20:43:46 -0500

    wip: Add remaining node types

--- src/lib/graph/node-types.ts
+++ src/lib/graph/node-types.ts
@@ -1,32 +1,80 @@
+/**
+ * @todo is this being used somewhere?
+ */
 export type NodeData = {
   isOutput?: boolean;
   operationVal?: string;
   outputType?: string;
 };
 
-type Node<TType extends string, TData extends { isOutput: boolean; [key: string]: unknown }> = {
+type Node<
+  TType extends string,
+  TData extends { [key: string]: unknown } = { [key: string]: never },
+> = {
   type: TType;
   data: TData;
 };
 
-// type TransformNode = Node<'transform', { isOutput: false }>;
-// type NoiseNode = Node<'noise', { isOutput: false }>;
-export type MathVec3 = Node<
-  'mathVec3',
-  {
-    isOutput: false;
-    operationVal: 'Add' | 'Sub' | 'Mult' | 'Div';
-  }
->;
-// type MixVec3Node = Node<'mixVec3', { isOutput: false }>;
-// type TerrainNode = Node<'terrain', { isOutput: true }>;
+export type Vector = Node<'vector'>;
+export type Transform = Node<'transform'>;
+export type Noise = Node<'noise', { mode: 'FBM' }>;
+export type MathFloat = Node<'mathFloat', { operationVal: 'Add' | 'Sub' | 'Mult' | 'Div' }>;
+export type MathVec3 = Node<'mathVec3', { operationVal: 'Add' | 'Sub' | 'Mult' | 'Div' }>;
+export type MixFloat = Node<'mixFloat'>;
+export type MixVec3 = Node<'mixVec3'>;
+export type Terrain = Node<'terrain'>;
 
-export type All = MathVec3;
+export type All =
+  | Vector
+  | Transform
+  | Noise
+  | MathFloat
+  | MathVec3
+  | MixFloat
+  | MixVec3
+  | Terrain;
 
+/**
+ * Handle IDs for each node type
+ */
 export const HANDLES = {
+  vector: {
+    in: {},
+    out: { result: 'vec3-out' },
+  },
+  transform: {
+    in: {
+      geo: 'geo-in',
+      translate: 'vec3-trans-in',
+      rotate: 'vec3-rotate-in',
+      scale: 'vec3-scale-in',
+      uniformScale: 'float-scale-in',
+    },
+    out: { result: 'geo-out' },
+  },
+  noise: {
+    in: { scale: 'float-scale', density: 'float-density' },
+    out: { result: 'float-out' },
+  },
+  mathFloat: {
+    in: { a: 'float-val1-in', b: 'float-val2-in' },
+    out: { result: 'float-out' },
+  },
   mathVec3: {
     in: { a: 'vec3-val1-in', b: 'vec3-val2-in' },
     out: { result: 'vec3-out' },
+  },
+  mixFloat: {
+    in: { a: 'float-val1-in', b: 'float-val2-in', mix: 'float-val3-in' },
+    out: { result: 'float-out' },
+  },
+  mixVec3: {
+    in: { a: 'vec3-val1-in', b: 'vec3-val2-in', mix: 'float-val3-in' },
+    out: { result: 'vec3-out' },
+  },
+  terrain: {
+    in: { height: 'float-trans-in' },
+    out: {},
   },
 } as const satisfies {
   [nodeType in All['type']]: { in: Record<string, string>; out: Record<string, string> };
@@ -34,42 +82,40 @@ export const HANDLES = {
 export type Handles = typeof HANDLES;
 
 export const BASE_NODES: All[] = [
-  // {
-  //   type: 'transform', // 0
-  //   data: { isOutput: false },
-  // },
-  // {
-  //   type: 'noise', // 1
-  //   data: { isOutput: false },
-  // },
+  {
+    type: 'transform', // 0
+    data: {},
+  },
+  {
+    type: 'noise', // 1
+    data: { mode: 'FBM' },
+  },
   {
     type: 'mathVec3', // 2
     data: {
-      isOutput: false,
       operationVal: 'Add',
     },
   },
-  // {
-  //   type: 'mixVec3', // 3
-  //   data: { isOutput: false },
-  // },
-  // {
-  //   type: 'terrain', // 4
-  //   data: { isOutput: true },
-  // },
-  // {
-  //   type: 'vector', // 5
-  //   data: { isOutput: false },
-  // },
-  // {
-  //   type: 'mathFloat', // 6
-  //   data: {
-  //     isOutput: false,
-  //     operationVal: 'add',
-  //   },
-  // },
-  // {
-  //   type: 'mixFloat', // 7
-  //   data: { isOutput: false },
-  // },
+  {
+    type: 'mixVec3', // 3
+    data: {},
+  },
+  {
+    type: 'terrain', // 4
+    data: {},
+  },
+  {
+    type: 'vector', // 5
+    data: {},
+  },
+  {
+    type: 'mathFloat', // 6
+    data: {
+      operationVal: 'Add',
+    },
+  },
+  {
+    type: 'mixFloat', // 7
+    data: {},
+  },
 ];
