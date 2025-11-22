fdfc9b65cc12472c97df7e0f52cda43a79d21df3
Thomas Shaw <printer.83mph@gmail.com>
2025-11-22 16:34:40 -0500

    Add offset prop on output handles

--- src/components/common/terraingen-node/handle-output.tsx
+++ src/components/common/terraingen-node/handle-output.tsx
@@ -6,9 +6,11 @@ import type * as types from './types';
 export type HandleOutputProps = {
   handleId: string;
   valueType: types.ValueType;
+  /** Vertical offset (integer) in case of multiple outputs */
+  offset?: number;
 };
 
-export default function HandleOutput({ handleId, valueType }: HandleOutputProps) {
+export default function HandleOutput({ handleId, valueType, offset = 0 }: HandleOutputProps) {
   const colorClassName = styles.VALUE_TYPE_CLASSNAMES[valueType];
 
   return (
@@ -16,7 +18,8 @@ export default function HandleOutput({ handleId, valueType }: HandleOutputProps)
       type="source"
       position={Position.Right}
       id={handleId}
-      className={`absolute! top-1/8! -right-2! h-3! w-3! -translate-y-1/2! ${colorClassName}`}
+      className={`absolute! top-[calc(var(--spacing)*(10+var(--vert-offset)*8))]! -right-2! h-3! w-3! -translate-y-1/2! ${colorClassName}`}
+      style={{ '--vert-offset': offset } as React.CSSProperties}
     />
   );
 }
