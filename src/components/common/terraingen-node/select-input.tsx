e6fca34dd98c4449fec2822b4d41b8895eb20522
Thomas Shaw <printer.83mph@gmail.com>
2025-11-25 11:11:41 -0500

    Revamp select input with radix select primitive

--- src/components/common/terraingen-node/select-input.tsx
+++ src/components/common/terraingen-node/select-input.tsx
@@ -1,3 +1,5 @@
+import * as Select from '@radix-ui/react-select';
+
 export type SelectInputProps<TValue extends string> = {
   value: TValue;
   onChange: (value: TValue) => void;
@@ -12,27 +14,29 @@ export default function SelectInput<TValue extends string>({
   label,
 }: SelectInputProps<TValue>) {
   return (
-    <div className="relative flex flex-col space-y-2 rounded-md bg-slate-700/50 p-3">
-      <div className="flex items-center justify-between">
-        <label className="text-sm font-medium">{label}</label>
-        <div className="inline-flex -translate-y-1 transform items-center gap-2 rounded-md bg-linear-to-r from-blue-600 to-green-600 px-4 py-2 font-bold text-white shadow-sm">
-          <select
-            value={value}
-            onChange={(evt) => onChange(evt.target.value as TValue)}
-            className="bg-transparent font-bold focus:outline-none"
-          >
-            {options.map((option) => (
-              <option
-                key={option.value}
-                value={option.value}
-                className="bg-slate-800 text-white"
-              >
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
+    <div className="relative flex items-center rounded-md bg-zinc-600/50 py-1 pr-1 pl-3">
+      <Select.Root value={value} onValueChange={(newValue) => onChange(newValue as TValue)}>
+        <label className="grow text-sm">{label}</label>
+        <Select.Trigger className="flex grow cursor-pointer justify-between rounded bg-transparent py-2 pr-2 pl-4 text-sm font-medium transition-colors hover:bg-zinc-600">
+          <Select.Value placeholder="Select..." />
+          <Select.Icon>▾</Select.Icon>
+        </Select.Trigger>
+        <Select.Portal>
+          <Select.Content>
+            <Select.Viewport className="rounded-lg border border-zinc-600 bg-zinc-900 p-1 text-white">
+              {options.map((option) => (
+                <Select.Item
+                  key={option.value}
+                  value={option.value}
+                  className="radix-highlighted:bg-linear-to-r radix-state-open:bg-zinc-800 radix-disabled:text-zinc-400 radix-disabled:grayscale radix-disabled:cursor-default cursor-pointer rounded-sm from-violet-600 to-indigo-600 px-3 py-1 focus-visible:outline-none"
+                >
+                  <Select.ItemText>{option.label}</Select.ItemText>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
     </div>
   );
 }
