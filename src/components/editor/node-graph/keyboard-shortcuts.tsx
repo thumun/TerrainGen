68db0aa6d8dc9898e8318447b2d4b31d9bd0fe58
Thomas Shaw <printer.83mph@gmail.com>
2025-12-01 16:00:07 -0500

    Add keyboard shortcuts component

---
+++ src/components/editor/node-graph/keyboard-shortcuts.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useReactFlow } from 'reactflow';
+
+import { useGraphGlobals } from '@/hooks/use-graph-globals';
+import { useMousePos } from '@/hooks/use-mouse-pos';
+
+export default function KeyboardShortcuts() {
+  const { getMousePos } = useMousePos();
+  const { createNode } = useGraphGlobals();
+  const { screenToFlowPosition } = useReactFlow();
+
+  // Attach keyboard shortcuts
+  useEffect(() => {
+    const onKeyDown = (evt: KeyboardEvent) => {
+      const mousePos = getMousePos();
+      const position = screenToFlowPosition(mousePos);
+
+      if (evt.key === 'v') {
+        createNode('vector', position);
+      } else if (evt.key === 'f') {
+        createNode('float', position);
+      } else if (evt.key === 'u') {
+        createNode('unsignedInt', position);
+      } else {
+        return;
+      }
+      evt.preventDefault();
+      evt.stopPropagation();
+    };
+    document.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
+  }, [createNode, getMousePos, screenToFlowPosition]);
+
+  return null;
+}
