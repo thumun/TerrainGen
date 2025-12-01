cb2af38601a1b77290cb5c657fddf99e6773b658
Thomas Shaw <printer.83mph@gmail.com>
2025-12-01 15:48:19 -0500

    Add useMousePos hook with context

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,10 +1,13 @@
 import Editor from '@/components/editor';
 import { GPUDeviceProvider } from '@/hooks/use-gpu-device';
+import { MousePosProvider } from '@/hooks/use-mouse-pos';
 
 export default function EditorPage() {
   return (
     <GPUDeviceProvider>
-      <Editor />
+      <MousePosProvider>
+        <Editor />
+      </MousePosProvider>
     </GPUDeviceProvider>
   );
 }
