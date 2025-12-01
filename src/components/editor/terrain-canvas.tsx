2df41e2570918461f18da539bff7b83c44f5890c
Thomas Shaw <printer.83mph@gmail.com>
2025-12-01 15:56:03 -0500

    Rework callbacks to allow pipeline updates from nodes

--- src/components/editor/terrain-canvas.tsx
+++ src/components/editor/terrain-canvas.tsx
@@ -5,12 +5,9 @@ import {
   TerrainRenderer,
   type TerrainRendererGlobalParameters,
 } from '@/lib/renderers/terrain-renderer';
-import type * as scene from '@/lib/scene';
 
 export type TerrainCanvasProps = {
   rendererRef: React.RefObject<TerrainRenderer | undefined>;
-  displacePipeline?: scene.DisplacePipeline;
-  instancingPipeline?: scene.InstancingPipeline;
   globalParams: TerrainRendererGlobalParameters;
 };
 
@@ -20,29 +17,7 @@ const createRenderer: WebGPUCanvasProps['createRenderer'] = async (webGPU, stage
   return renderer;
 };
 
-export default function TerrainCanvas({
-  rendererRef,
-  displacePipeline,
-  instancingPipeline,
-  globalParams,
-}: TerrainCanvasProps) {
-  // Update pipelines etc when scene graph changes
-  useEffect(() => {
-    if (displacePipeline === undefined) {
-      rendererRef.current?.disableDisplacePipeline();
-    } else {
-      rendererRef.current?.configureDisplacePipeline(displacePipeline);
-    }
-  }, [displacePipeline, rendererRef]);
-
-  useEffect(() => {
-    if (instancingPipeline === undefined) {
-      rendererRef.current?.disableInstancingPipeline();
-    } else {
-      void rendererRef.current?.configureInstancingPipeline(instancingPipeline);
-    }
-  }, [instancingPipeline, rendererRef]);
-
+export default function TerrainCanvas({ rendererRef, globalParams }: TerrainCanvasProps) {
   // Update global parameters when sliders update them
   useEffect(() => {
     rendererRef.current?.setMeshUniforms(globalParams.size, globalParams.resolution);
