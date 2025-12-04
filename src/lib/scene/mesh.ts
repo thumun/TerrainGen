4987007274e84d53dde7ddda37b4137f3be19184
Raymond Feng <113264095+sgmq0@users.noreply.github.com>
2025-12-04 01:31:24 -0500

    lint & format

--- src/lib/scene/mesh.ts
+++ src/lib/scene/mesh.ts
@@ -1,6 +1,5 @@
-import { GLTFLoader, type GLTFWithBuffers, type GLTF, type GLTFMesh, type GLTFMeshPrimitive, type GLTFMaterial, type GLTFSampler } from '@loaders.gl/gltf';
-import { forEach, load, parse } from '@loaders.gl/core';
-import type { Vec3 } from 'wgpu-matrix';
+import { load } from '@loaders.gl/core';
+import { GLTFLoader, type GLTFWithBuffers, type GLTF } from '@loaders.gl/gltf';
 
 // storage class for mesh uniforms
 class MeshUniforms {
@@ -18,13 +17,12 @@ class MeshUniforms {
 }
 
 const numComponents: { [key: string]: number } = {
-  "SCALAR": 1,
-  "VEC2": 2,
-  "VEC3": 3,
-  "VEC4": 4,
-  "MAT4": 16,
+  SCALAR: 1,
+  VEC2: 2,
+  VEC3: 3,
+  VEC4: 4,
+  MAT4: 16,
 };
-
 
 // template class
 export abstract class Mesh {
@@ -196,12 +194,13 @@ export class OBJ extends Mesh {
     this.indices = new Uint32Array(finalIndices);
   }
 
-  async loadGltf(url: string): Promise<{ gltfWithBuffers: GLTFWithBuffers; gltf: GLTF; }> {
-    console.log("load gltf");
-    const gltfWithBuffers = (await load(url, GLTFLoader)) as GLTFWithBuffers;
+  async loadGltf(url: string): Promise<{ gltfWithBuffers: GLTFWithBuffers; gltf: GLTF }> {
+    console.log('load gltf');
+    const gltfWithBuffers = await load(url, GLTFLoader);
     const gltf = gltfWithBuffers.json;
     return {
-      gltfWithBuffers, gltf
+      gltfWithBuffers,
+      gltf,
     };
   }
 
@@ -210,35 +209,46 @@ export class OBJ extends Mesh {
     const finalIndices: number[] = [];
 
     for (const mesh of gltf.meshes!) {
-
       for (const prim of mesh.primitives) {
-
         // load positions
-        const posAccessor = gltf.accessors![prim.attributes["POSITION"]];
+        const posAccessor = gltf.accessors![prim.attributes['POSITION']];
         const posBufferView = gltf.bufferViews![posAccessor.bufferView!];
-        const posBuffer = gltfWithBuffers.buffers[posBufferView.buffer]
+        const posBuffer = gltfWithBuffers.buffers[posBufferView.buffer];
 
-        const posByteOffset = (posBufferView.byteOffset ?? 0) + (posAccessor.byteOffset ?? 0) + posBuffer.byteOffset;
+        const posByteOffset =
+          (posBufferView.byteOffset ?? 0) +
+          (posAccessor.byteOffset ?? 0) +
+          posBuffer.byteOffset;
         const posArrayLength = posAccessor.count * numComponents[posAccessor.type];
-        const positions = new Float32Array(posBuffer.arrayBuffer, posByteOffset, posArrayLength);
+        const positions = new Float32Array(
+          posBuffer.arrayBuffer,
+          posByteOffset,
+          posArrayLength,
+        );
 
         // load normals
-        const norAccessor = gltf.accessors![prim.attributes["NORMAL"]];
+        const norAccessor = gltf.accessors![prim.attributes['NORMAL']];
         const norBufferView = gltf.bufferViews![norAccessor.bufferView!];
-        const norBuffer = gltfWithBuffers.buffers[norBufferView.buffer]
+        const norBuffer = gltfWithBuffers.buffers[norBufferView.buffer];
 
-        const norByteOffset = (norBufferView.byteOffset ?? 0) + (norAccessor.byteOffset ?? 0) + norBuffer.byteOffset;
+        const norByteOffset =
+          (norBufferView.byteOffset ?? 0) +
+          (norAccessor.byteOffset ?? 0) +
+          norBuffer.byteOffset;
         const norArrayLength = norAccessor.count * numComponents[norAccessor.type];
         const normals = new Float32Array(norBuffer.arrayBuffer, norByteOffset, norArrayLength);
 
         // load uvs
         let uvs: Float32Array;
-        if (prim.attributes["TEXCOORD_0"]) {
-          const uvAccessor = gltf.accessors![prim.attributes["TEXCOORD_0"]];
+        if (prim.attributes['TEXCOORD_0']) {
+          const uvAccessor = gltf.accessors![prim.attributes['TEXCOORD_0']];
           const uvBufferView = gltf.bufferViews![uvAccessor.bufferView!];
-          const uvBuffer = gltfWithBuffers.buffers[uvBufferView.buffer]
+          const uvBuffer = gltfWithBuffers.buffers[uvBufferView.buffer];
 
-          const uvByteOffset = (uvBufferView.byteOffset ?? 0) + (uvAccessor.byteOffset ?? 0) + norBuffer.byteOffset;
+          const uvByteOffset =
+            (uvBufferView.byteOffset ?? 0) +
+            (uvAccessor.byteOffset ?? 0) +
+            norBuffer.byteOffset;
           const uvArrayLength = uvAccessor.count * numComponents[uvAccessor.type];
           uvs = new Float32Array(uvBuffer.arrayBuffer, uvByteOffset, uvArrayLength);
         } else {
@@ -254,7 +264,10 @@ export class OBJ extends Mesh {
         const idxBufferView = gltf.bufferViews![idxAccessor.bufferView!];
         const idxBuffer = gltfWithBuffers.buffers[idxBufferView.buffer];
 
-        const idxOffset = (idxBufferView.byteOffset ?? 0) + (idxAccessor.byteOffset ?? 0) + idxBuffer.byteOffset;
+        const idxOffset =
+          (idxBufferView.byteOffset ?? 0) +
+          (idxAccessor.byteOffset ?? 0) +
+          idxBuffer.byteOffset;
 
         let idxArray: Uint16Array | Uint32Array | Uint8Array | undefined;
         switch (idxAccessor.componentType) {
@@ -274,9 +287,14 @@ export class OBJ extends Mesh {
         // push these guys into the final array
         for (let i = 0; i < positions.length / 3; i++) {
           finalVertices.push(
-            positions[3 * i], positions[3 * i + 1], positions[3 * i + 2],
-            normals[3 * i], normals[3 * i + 1], normals[3 * i + 2],
-            uvs[2 * i], uvs[2 * i + 1]
+            positions[3 * i],
+            positions[3 * i + 1],
+            positions[3 * i + 2],
+            normals[3 * i],
+            normals[3 * i + 1],
+            normals[3 * i + 2],
+            uvs[2 * i],
+            uvs[2 * i + 1],
           );
         }
         for (let i = 0; i < idxArray!.length; i++) {
@@ -288,5 +306,4 @@ export class OBJ extends Mesh {
     this.vertices = new Float32Array(finalVertices);
     this.indices = new Uint32Array(finalIndices);
   }
-
 }
