0c8a4f362261e27452badf0754ea38608e8d3511
Raymond Feng <113264095+sgmq0@users.noreply.github.com>
2025-12-07 20:51:47 -0500

    Update HDRImageData.ts

--- src/lib/scene/io-rgbe-main/src/HDRImageData.ts
+++ src/lib/scene/io-rgbe-main/src/HDRImageData.ts
@@ -2,9 +2,9 @@
  * HDRImageData contains all decompressed image data.
  */
 export class HDRImageData {
-  width: number;
-  height: number;
-  exposure: number;
-  gamma: number;
-  data: Float32Array;
+  width!: number;
+  height!: number;
+  exposure!: number;
+  gamma!: number;
+  data!: Float32Array;
 }
